6c11028d946d50160dea7b89a2e55df6c021d2bc
Nikhilgarepalli <105476990+Nikhilgarepalli@users.noreply.github.com>
2024-08-04 12:17:20 +0530

    Delete GoKapture directory

--- GoKapture/src/main/java/dev/nikhil/gokapture/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.nikhil.gokapture.repository;
-
-
-import dev.nikhil.gokapture.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
